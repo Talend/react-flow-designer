7dedd9be3fb41d169cd1658e5bd66dbed1da693e
Axel CATELAND <acateland@talend.com>
2016-08-19 13:37:41 +0200

    fix a test on LinkRenderer component

--- src/components/link/LinkRenderer.test.js
+++ src/components/link/LinkRenderer.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Map } from 'immutable';
+import { Map, OrderedMap } from 'immutable';
 
 import LinksRenderer from './LinksRenderer.component';
-import { LinkRecord } from '../../constants/flowdesigner.model';
+import { LinkRecord, PortRecord, PositionRecord } from '../../constants/flowdesigner.model';
 
 const MockLink = () => (
   <span>MockLink</span>
@@ -14,14 +14,25 @@ describe('<LinksRenderer /> renders correctly', () => {
     it('<LinksRenderer /> renders correctly', () => {
         const links = new Map().set('id', new LinkRecord({
             id: 'id',
+            sourceId: 'port1',
+            targetId: 'port2',
             linkType: 'id',
 
+        }));
+        const ports = new OrderedMap()
+        .set('port1', new PortRecord({
+            id: 'port1',
+            position: new PositionRecord({ x: 100, y: 100 }),
+        }))
+        .set('port2', new PortRecord({
+            id: 'port2',
+            position: new PositionRecord({ x: 200, y: 200 }),
         }));
         const linkTypeMap = {
             id: { id: 'id', component: MockLink },
         };
         const tree = renderer.create(
-          <LinksRenderer links={links} linkTypeMap={linkTypeMap} />
+          <LinksRenderer links={links} ports={ports} linkTypeMap={linkTypeMap} />
         ).toJSON();
         expect(tree).toMatchSnapshot();
     });
