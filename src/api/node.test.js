ca5de5f987ceac38bec7a29806c7c8911fb23259
acateland <acateland@talend.com>
2018-07-12 13:59:23 +0200

    more tests, some failling

--- src/api/node.test.js
+++ src/api/node.test.js
@@ -44,6 +44,8 @@ describe('Node', () => {
 	const size = Size.create(50, 80);
 	const nodeType = 'NodeType';
 	const testNode = Node.create(id, position, size, nodeType);
+	const key = 'KEY';
+	const value = { whatever: 'whatever' };
 
 	const improperId = 34;
 	const improperPosition = new Immutable.Map({ x: 10, y: 10 });
@@ -143,69 +145,228 @@ describe('Node', () => {
 			expect(() => Node.setPosition(improperPosition, testNode)).toThrow();
 		});
 		it('throw given an improper Node', () => {
+			// given
+			// when
+			// expect
 			expect(() => Node.setPosition(position, improperNode)).toThrow();
 		});
 	});
 	describe('getSize', () => {
-		it('given a proper Node return a Size');
-		it('throw given an improper node');
+		it('given a proper Node return a Size', () => {
+			// given
+			// when
+			const test = Node.getSize(testNode);
+			// expect
+			expect(test).toEqual(size);
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.getSize(improperNode)).toThrow();
+		});
 	});
 	describe('setSize', () => {
-		it('given a proper Node and Size return a Node with updated Size');
-		it('throw given an improper Size');
-		it('throw given an improper Node');
+		it('given a proper Node and Size return a Node with updated Size', () => {
+			// given
+			const newSize = Size.create(100, 100);
+			// when
+			const test = Node.setSize(newSize, testNode);
+			// expect
+			expect(Node.getSize(test)).toEqual(newSize);
+		});
+		it('throw given an improper Size', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.setSize(improperSize, testNode)).toThrow();
+		});
+		it('throw given an improper Node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.setSize(size, improperNode)).toThrow();
+		});
 	});
 	describe('getComponentType', () => {
-		it('given a proper Node return a ComponentType');
-		it('throw given an improper node');
+		it('given a proper Node return a ComponentType', () => {
+			// given
+			// when
+			const test = Node.getComponentType(testNode);
+			// expect
+			expect(test).toEqual(nodeType);
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.getComponentType(improperNode)).toThrow();
+		});
 	});
 	describe('setComponentType', () => {
-		it('given a proper Node and ComponentType return a Node with updated ComponentType');
-		it('throw given an improper ComponentType');
-		it('throw given an improper Node');
+		it('given a proper Node and ComponentType return a Node with updated ComponentType', () => {
+			// given
+			const newComponentType = 'squareOne';
+			// when
+			const test = Node.setComponentType(newComponentType, testNode);
+			// expect
+			expect(Node.getComponentType(test)).toEqual(newComponentType);
+		});
+		it('throw given an improper ComponentType', () => {
+			// given
+			const newComponentType = { type: 'squareOne' };
+			// when
+			// expect
+			expect(() => Node.setComponentType(newComponentType, testNode)).toThrow();
+		});
+		it('throw given an improper Node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.setComponentType(nodeType, improperNode)).toThrow();
+		});
 	});
 	describe('setData', () => {
-		it('given a proper key, value and node return said node with the new key/value');
-		it('throw given an improper key');
-		it('throw given an improper node');
+		it('given a proper key, value and node return said node with the new key/value', () => {
+			// given
+			const newKey = 'newKey';
+			const newValue = 'newValue';
+			// when
+			const test = Node.setData(newKey, newValue, testNode);
+			// expect
+			expect(Node.getData(newKey, test)).toEqual(newValue);
+		});
+		it('throw given an improper key', () => {
+			// given
+			const improperKey = 12;
+			// when
+			// expect
+			expect(() => Node.setData(improperKey, value, testNode)).toThrow();
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.setData(key, value, improperNode)).toThrow();
+		});
 	});
 	describe('getData', () => {
-		it('given a proper key and node return value associated with the key');
-		it('throw given an improper key');
-		it('throw given an improper node');
+		it('given a proper key and node return value associated with the key', () => {
+			// given
+			const newKey = 'newKey';
+			const newValue = 'newValue';
+			const preparedNode = Node.setData(newKey, newValue, testNode);
+			// when
+			const test = Node.getData(newKey, preparedNode);
+			// expect
+			expect(test).toEqual(newValue);
+		});
+		it('throw given an improper key', () => {
+			// given
+			const improperKey = 12;
+			// when
+			// expect
+			expect(() => Node.getData(improperKey, testNode)).toThrow();
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.getData(key, improperNode)).toThrow();
+		});
 	});
 	describe('hasData', () => {
-		it('given a proper key and node return true if key exist');
-		it('throw given an improper key');
-		it('throw given an improper node');
+		it('given a proper key and node return true if key exist', () => {
+			// given
+			const newKey = 'newKey';
+			const newValue = 'newValue';
+			const preparedNode = Node.setData(newKey, newValue, testNode);
+			// when
+			const test = Node.hasData(newKey, preparedNode);
+			// expect
+			expect(test).toEqual(true);
+		});
+		it('throw given an improper key', () => {
+			// given
+			const improperKey = 12;
+			// when
+			// expect
+			expect(() => Node.hasData(improperKey, testNode)).toThrow();
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.hasData(key, improperNode)).toThrow();
+		});
 	});
 	describe('deleteData', () => {
-		it('given a proper key and node return node without the key in data property if key exist');
-		it('throw given an improper key');
-		it('throw given an improper node');
+		it('given a proper key and node return node without the key in data property if key exist', () => {
+			// given
+			const newKey = 'newKey';
+			const newValue = 'newValue';
+			const preparedNode = Node.setData(newKey, newValue, testNode);
+			// when
+			const test = Node.deleteData(newKey, preparedNode);
+			// expect
+			expect(Node.hasData(newKey, test)).toEqual(false);
+		});
+		it('throw given an improper key', () => {
+			// given
+			const improperKey = 12;
+			// when
+			// expect
+			expect(() => Node.deleteData(improperKey, testNode)).toThrow();
+		});
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.deleteData(key, improperNode)).toThrow();
+		});
 	});
 	describe('setGraphicalAttribute', () => {
 		it('given a proper key, value and node return said node with the new key/value');
 		it('throw given a key being part of FORBIDEN_GRAPHICAL_ATTRIBUTES');
 		it('throw given an improper key');
-		it('throw given an improper node');
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.setGraphicalAttribute(key, value, improperNode)).toThrow();
+		});
 	});
 	describe('getGraphicalAttribute', () => {
 		it('given a proper key and node return value associated with the key');
 		it('throw given a key being part of FORBIDEN_GRAPHICAL_ATTRIBUTES');
 		it('throw given an improper key');
-		it('throw given an improper node');
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.getGraphicalAttribute(key, improperNode)).toThrow();
+		});
 	});
 	describe('hasGraphicalAttribute', () => {
 		it('given a proper key and node return true if key exist');
 		it('throw given a key being part of FORBIDEN_GRAPHICAL_ATTRIBUTES');
 		it('throw given an improper key');
-		it('throw given an improper node');
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.hasGraphicalAttribute(key, improperNode)).toThrow();
+		});
 	});
 	describe('deleteGraphicalAttribute', () => {
 		it('given a proper key and node return node without the key in data property if key exist');
 		it('throw given a key being part of FORBIDEN_GRAPHICAL_ATTRIBUTES');
 		it('throw given an improper key');
-		it('throw given an improper node');
+		it('throw given an improper node', () => {
+			// given
+			// when
+			// expect
+			expect(() => Node.deleteGraphicalAttribute(key, improperNode)).toThrow();
+		});
 	});
 });
