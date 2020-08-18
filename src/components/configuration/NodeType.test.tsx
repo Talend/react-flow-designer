c8b9cd41d1041f339ca82b8505b68a5db0116986
Mathieu Huchet <mhuchet@talend.com>
2020-08-18 11:30:48 +0200

    Run prettier on all files

--- src/components/configuration/NodeType.test.tsx
+++ src/components/configuration/NodeType.test.tsx
@@ -5,9 +5,11 @@ import NodeType from './NodeType.component';
 const mockComponent = () => <div />;
 
 describe('Testing <NodeType>', () => {
-  it('should log an error if rendered', () => {
-    expect(() => {
-      shallow(<NodeType type="string" component={mockComponent} />);
-    }).toThrowError('<NodeType> elements are for DataFlow configuration only and should not be rendered');
-  });
+	it('should log an error if rendered', () => {
+		expect(() => {
+			shallow(<NodeType type="string" component={mockComponent} />);
+		}).toThrowError(
+			'<NodeType> elements are for DataFlow configuration only and should not be rendered',
+		);
+	});
 });
